7aa48b7a800891c7cb53d7a5a9dc91b2f894c401
Alex <kavcoffeefox@yandex.ru>
2019-03-31 11:10:47 +0300

    Привел задачу number_sum в соответствие с pip8

--- number_sum/solution.py
+++ number_sum/solution.py
@@ -5,7 +5,7 @@ sum = 0
 for number in input_str:
     try:
         sum += int(number)
-    except:
+    except Exception:
         print(number, " - Is`s not number!!! Egnore this symbol")
 
-print( sum )
+print(sum)
